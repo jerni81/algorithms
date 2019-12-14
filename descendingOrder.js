68980d0e489bac4f9c43b7504a6dd41ccdd9bd99
jerni81 <jerni81@yahoo.com>
2019-12-14 14:42:20 -0500

    algo 2 descendingOrder

---
+++ descendingOrder.js
@@ -0,0 +1,22 @@
+//Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+let descendingOrder = (num) => {
+  let str = num.toString()
+  let arr = str.split('')
+  for (let i = 0; i <arr.length; i++){
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        let holder = arr[i];
+        arr[i] = arr[j];
+        arr[j] = holder
+      }
+    }
+
+  // let outPut = str2.parseInt()
+  }
+  let str2 = arr.join('')
+  // let output = valueOf(str2)
+  console.log(str2);
+}
+
+descendingOrder(283950);
--- isogram.js
+++ isogram.js
@@ -1,3 +1,7 @@
+// This algo is to determine if the input word is an isogram or not, it compares each letter in the string to find if any repeat themself
+
+
+
 let isogram = (word) => {
   let answer = true
   let lowerWord = word.toLowerCase()
