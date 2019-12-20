9a322b5a0704f7fb36236acf3e7735ccacbcd1d3
jerni81 <jerni81@yahoo.com>
2019-12-20 10:49:57 -0500

    code cleanup

--- queueTime.js
+++ queueTime.js
@@ -1,19 +1,25 @@
+// There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total time required for all the customers to check out!
+//
+// input
+// customers: an array of positive integers representing the queue. Each integer represents a customer, and its value is the amount of time they require to check out.
+// n: a positive integer, the number of checkout tills.
+// output
+// The function should return an integer, the total time required.
+
 let queueTime = (customer, n) => {
-  let lines = {}
+  let lines = {};
   while (n > 0) {
-    lines[n] = 0
-    n = n -1
+    lines[n] = 0;
+    n = n - 1;
   }
-  for (let i = 0; i < customer.length; i ++) {
-  lines[Object.keys(lines)[Object.values(lines).indexOf(Math.min(...Object.values(lines)))]] += customer[i]
+  for (let i = 0; i < customer.length; i++) {
+    lines[
+      Object.keys(lines)[
+        Object.values(lines).indexOf(Math.min(...Object.values(lines)))
+      ]
+    ] += customer[i];
   }
   console.log(Math.max(...Object.values(lines)));
-  // if (Math.max(...customer) > time/n) {
-  //   console.log(Math.max(...customer))
-  // } else {
-  //   console.log(time/n)
-  //
-  // }
-}
+};
 
-queueTime([1,2,3,4], 3)
+queueTime([1, 2, 3, 4], 3);
